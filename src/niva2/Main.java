cf388ebea85f499d7156dc10a63f76fc06a69e52
Medou <leveritable1977@gmail.com>
2026-09-23 09:18:19 +0200

    Klar med niva 1 och niva 2

--- src/niva2/Main.java
+++ src/niva2/Main.java
@@ -9,7 +9,7 @@ public class Main {
         DoorSensor dörr = new DoorSensor(1, 5);
         System.out.println("\n[STARTLÄGE] Dörr öppen: " + dörr.isOpen + " | Lampa tänd: " + lampa.isOn);
 
-        // 2. Din dörraktivering (Toggle)
+        // 2. Dörraktivering (Toggle)
         System.out.println("\n>> HÄNDELSE: Någon öppnar dörren i zon " + dörr.zone);
         dörr.isOpen = true;
         if (dörr.isOpen) {
@@ -19,16 +19,24 @@ public class Main {
         System.out.println("\n------------------------------------------------");
         System.out.println(">> HÄNDELSE: Startar dimmerloopen (0% till 100%)");
 
-        // 3. UTMANING: Dimmerloop som stegar med 20 i taget (i += 20)
+        // 3. UTMANING: Dimmerloop som ändrar brightness direkt och stegar med 20
         for (int i = 0; i <= 100; i += 20) {
-            lampa.setBrightness(i);
+            lampa.brightness = i;
+            System.out.println("[INFO] Ljusstyrkan har ändrats till " + lampa.brightness + "%.");
         }
 
         System.out.println("\n------------------------------------------------");
         System.out.println(">> HÄNDELSE: Testar felhanteringen med ett ogiltigt värde (-50)");
 
-        // 4. UTMANING: Validera ogiltigt värde via else i settern
-        lampa.setBrightness(-50);
+        // 4. UTMANING: Validera värdet direkt och hantera fel via else
+        int testVarde = -50;
+        if (testVarde >= 0 && testVarde <= 100) {
+            lampa.brightness = testVarde;
+            System.out.println("[INFO] Ljusstyrkan ändrades till: " + lampa.brightness + "%.");
+        } else {
+            // Här är felhanteringen som efterfrågas!
+            System.out.println("[FEL] Ljusstyrkan måste vara mellan 0 och 100. Värdet " + testVarde + " avvisades.");
+        }
 
         System.out.println("\n--- TESTET AV NIVÅ 2 ÄR COMPLETED ---");
     }
--- src/niva2/SmartLight.java
+++ src/niva2/SmartLight.java
@@ -11,7 +11,7 @@ public class SmartLight {
         this.brightness = 0;
     }
 
-    // Toggle-metoden (Vänder på strömbrytaren)
+    // Toggle-metoden (Ändrar strömbrytaren)
     public void toggle() {
         this.isOn = !this.isOn;
         if (this.isOn) {
@@ -22,28 +22,5 @@ public class SmartLight {
             System.out.println("[LAMP-METOD] Lampan med Id " + deviceId + " har SLÄCKTS.");
         }
     }
-
-    // 3 Getters (Utan "this." för renare kod)
-    public int getDeviceId() {
-        return deviceId;
-    }
-
-    public boolean isOn() {
-        return isOn; // Använder "is" istället för "get" för boolean
-    }
-
-    public int getBrightness() {
-        return brightness;
-    }
-
-    // 1 Setter (Med "this." på grund av namnkrock och felhantering)
-    public void setBrightness(int brightness) {
-        if (brightness >= 0 && brightness <= 100) {
-            this.brightness = brightness;
-            System.out.println("[INFO] Ljusstyrkan har ändrats till " + this.brightness + "%.");
-        } else {
-            // Här är felhanteringen via else som uppgiften ber om!
-            System.out.println("[FEL] Ljusstyrkan måste vara mellan 0 och 100.");
-        }
-    }
 }
+
